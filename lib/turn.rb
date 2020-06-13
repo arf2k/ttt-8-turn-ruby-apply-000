981feb1f44dcc41e45d61b6dfd51efad8691dae6
Alex Foreman <foreman.alexander@gmail.com>
2020-06-13 00:15:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,11 @@ end
 def turn(board)
   puts "Please enter 1-9:"
 end 
+
+def input_to_index(user_input)
+  user_input.to_i - 1  
+end 
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
